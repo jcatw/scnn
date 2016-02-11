6fe7cdbb67355298dde2272b1fb47ea6eef853f9
James Atwood <jatwood@cs.umass.edu>
2016-02-11 17:15:56 -0500

    lasagne dependency updated to 0.1dev

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1.0',
+    version='1.1.1',
 
     description='Search-Convolutional Neural Networks',
     long_description=long_description,
@@ -73,7 +73,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['numpy',
                       'theano',
-                      'lasagne',
+                      'lasagne>=0.1dev',
                       'matplotlib',
                       ],
 
