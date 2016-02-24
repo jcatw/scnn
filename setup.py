117f1338c693982eb427b918fda1e9b711fa2467
James Atwood <jatwood@cs.umass.edu>
2016-02-24 13:29:48 -0500

    fixed installation bug

--- setup.py
+++ setup.py
@@ -9,20 +9,60 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='scnn',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1.2',
+    version='1.1.5',
 
     description='Search-Convolutional Neural Networks',
-    long_description=long_description,
+    long_description="""
+SCNN
+====
+
+An implementation of search-convolutional neural networks [1], a new model for graph-structured data.
+
+Installation
+------------
+Using pip:
+
+    pip install scnn
+
+Usage
+-----
+
+	import numpy as np
+    from scnn import SCNN, data
+    from sklearn.metrics import f1_score
+
+	# Parse the cora dataset and return an adjacency matrix, a design matrix, and a 1-hot label matrix
+    A, X, Y = data.parse_cora()
+
+	# Construct array indices for the training, validation, and test sets
+    n_nodes = A.shape[0]
+    indices = np.arange(n_nodes)
+    train_indices = indices[:n_nodes // 3]
+    valid_indices = indices[n_nodes // 3:(2* n_nodes) // 3]
+    test_indices  = indices[(2* n_nodes) // 3:]
+
+	# Instantiate an SCNN and fit it to cora
+    scnn = SCNN()
+    scnn.fit(A, X, Y, train_indices=train_indices, valid_indices=valid_indices)
+
+	# Predict labels for the test set
+    preds = scnn.predict(A, X, test_indices)
+    actuals = np.argmax(Y[test_indices,:], axis=1)
+
+	# Display performance
+    print 'F score: %.4f' % (f1_score(actuals, preds))
+
+References
+----------
+
+[1] http://arxiv.org/abs/1511.02136
+    """,
 
     # The project's main homepage.
     url='https://github.com/jcatw/scnn',
@@ -86,7 +126,7 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={'scnn': ['scnn/data/cora', 'scnn/data/nci']},
+    package_data={'scnn': ['README.md','scnn/data/cora', 'scnn/data/nci']},
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
