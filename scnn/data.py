02931f92335507f169e8627f78ceb8e10b1de68d
James Atwood <jatwood@cs.umass.edu>
2016-12-28 11:21:39 -0500

    cleaner data

--- scnn/data.py
+++ scnn/data.py
@@ -8,7 +8,6 @@ import os
 
 current_dir = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
 
-
 def parse_cora(plot=False):
     path = "%s/data/cora/" % (current_dir,)
 
@@ -146,17 +145,27 @@ def parse_pubmed():
 
 
 def parse_nci(graph_name='nci1.graph', with_structural_features=False):
-    path = "%s/data/nci/" % (current_dir,)
+    path = "%s/data/" % (current_dir,)
 
     if graph_name == 'nci1.graph':
         maxval = 37
+        n_classes = 2
     elif graph_name == 'nci109.graph':
         maxval = 38
+        n_classes = 2
+    elif graph_name == 'mutag.graph':
+        maxval = 7
+        n_classes = 2
+    elif graph_name == 'ptc.graph':
+        maxval = 22
+        n_classes = 2
+    elif graph_name == 'enzymes.graph':
+        maxval = 3
+        n_classes = 6
 
     with open(path+graph_name,'r') as f:
         raw = cp.load(f)
 
-        n_classes = 2
         n_graphs = len(raw['graph'])
 
         A = []
@@ -165,7 +174,14 @@ def parse_nci(graph_name='nci1.graph', with_structural_features=False):
 
         for i in range(n_graphs):
             # Set label
-            Y[i][raw['labels'][i]] = 1
+            class_label = raw['labels'][i]
+            if n_classes == 2:
+                if class_label == 1:
+                    Y[i,1] = 1
+                else:
+                    Y[i,0] = 1
+            else:
+                Y[i, class_label-1] = 1
 
             # Parse graph
             G = raw['graph'][i]
@@ -176,12 +192,15 @@ def parse_nci(graph_name='nci1.graph', with_structural_features=False):
             x = np.zeros((n_nodes,maxval), dtype='float32')
 
             for node, meta in G.iteritems():
-                x[node,meta['label'][0] - 1] = 1
+                label = meta['label'][0] - 1
+                x[node, label] = 1
                 for neighbor in meta['neighbors']:
                     a[node, neighbor] = 1
 
             A.append(a)
             rX.append(x)
+
+    print Y.sum(0)
 
     if with_structural_features:
         import networkx as nx
@@ -203,233 +222,5 @@ def parse_nci(graph_name='nci1.graph', with_structural_features=False):
             rX[i] = np.hstack((rX[i],struct_feat))
 
     return A, rX, Y
-
-
-def parse_blogcatalog(target=1):
-    path = "%s/data/blogcatalog/" % (current_dir,)
-
-    n_nodes = 10312
-    n_features = 38
-    n_classes = 2
-
-    X = np.zeros((n_nodes, n_features), dtype='float32')
-    Y = np.zeros((n_nodes, n_classes), dtype='int32')
-
-    # parse features
-    with open(path+'blogcatalog_features.csv','r') as f:
-        for i,line in enumerate(f.xreadlines()):
-            items = np.asarray([int(x) for x in line.strip().split(',')])
-            X[i,:] = np.hstack([items[:target], items[target+1:]])
-            Y[i,items[target]] = 1
-
-    # parse graph
-    A = np.genfromtxt(path+'blogcatalog_network.csv', delimiter=',').astype('float32')
-
-    return A, X, Y
-
-def parse_wikirfa(n_features = 100):
-    '''Parse wiki-rfa for an edge sentiment analysis experiment.'''
-
-    #from sklearn.feature_extraction.text import CountVectorizer
-    from sklearn.feature_extraction.text import HashingVectorizer
-
-    path = "%s/data/wikirfa/" % (current_dir,)
-
-    n_nodes = 10926
-    n_edges = 176963  # number of +/- edges
-
-    n_edges_read = 189004  # includes neutral edges
-
-    # username -> index map constructed on the fly
-    usermap = {}
-
-    A = np.zeros((n_nodes, n_nodes), dtype='float32')
-    B = np.zeros((n_edges, n_nodes), dtype='float32')
-    X_N = np.ones((n_nodes, 1) , dtype='float32')
-    X_E = np.ones((n_edges, n_features) , dtype='float32')
-    Y = np.zeros((n_edges, 2), dtype='int32')
-
-    # build comment vectorizer
-    with open(path+'all_comments.txt','r') as f:
-        #vectorizer = CountVectorizer('content', max_features=n_features)
-        vectorizer = HashingVectorizer('content', n_features=n_features)
-        vectorizer.fit(f.xreadlines())
-
-    u = 0
-    v = 0
-
-    resmap  = {-1: 0, 1: 1}
-    votemap = {-1: 0, 1: 1}
-
-    with open(path + 'wiki-RfA.txt','r') as f:
-        for i in range(n_edges_read):
-            # Read in the entry
-            # SRC:Guettarda
-            tail = f.readline().strip()[4:]
-
-            # TGT:Lord Roem
-            head = f.readline().strip()[4:]
-
-            # VOT:1
-            vote = int(f.readline().strip()[4:])
-
-            # RES:1
-            res = int(f.readline().strip()[4:])
-
-            # YEA:2013
-            year = int(f.readline().strip()[4:])
-
-            #DAT:19:53, 25 January 2013
-            date = f.readline().strip()[4:]
-
-            # TXT:'''Support''' per [[WP:DEAL]]: clueful, and unlikely to break Wikipedia.
-            txt = f.readline().strip()[4:]
-
-            # kill blank line
-            f.readline()
-
-            # Process the entry
-            # index users
-            if tail not in usermap:
-                usermap[tail] = u
-                u += 1
-            if head not in usermap:
-                usermap[head] = u
-                u += 1
-
-            if vote != 0:
-                # add vote edge
-                A[usermap[tail], usermap[head]] = vote
-                A[usermap[head], usermap[tail]] = vote
-
-                # add to incidence matrix
-                B[v, usermap[tail]] = 1
-                B[v, usermap[head]] = 1
-
-                X_N[usermap[head],0] = resmap[res]
-                X_E[v, :] = np.asarray(vectorizer.transform([txt]).todense())[0]
-
-                Y[v, votemap[vote]] = 1
-                v += 1
-
-    print u
-    print v
-
-    assert len(usermap) == n_nodes
-    assert u == n_nodes
-    assert v == n_edges
-
-    return A, B, X_N, X_E, Y
-
-def parse_wikirfa_edges_as_search(n_features = 100):
-    '''Parse wiki-rfa for an edge sentiment analysis experiment.'''
-
-    from sklearn.feature_extraction.text import HashingVectorizer
-
-    path = "%s/data/wikirfa/" % (current_dir,)
-
-    n_nodes = 10926
-    n_edges = 176963  # number of +/- edges
-
-    n_edges_read = 189004  # includes neutral edges
-
-    # username -> index map constructed on the fly
-    usermap = {}
-
-    A = np.zeros((n_features + 1, n_nodes, n_nodes), dtype='float32')
-    B = np.zeros((n_edges, n_nodes), dtype='float32')
-    X = np.ones((n_nodes, 1) , dtype='float32')
-    Y = np.zeros((n_edges, 2), dtype='int32')
-
-    # build comment vectorizer
-    with open(path+'all_comments.txt','r') as f:
-        vectorizer = HashingVectorizer('content', n_features=n_features)
-        vectorizer.fit(f.xreadlines())
-
-    u = 0
-    v = 0
-
-    resmap  = {-1: 0, 1: 1}
-    votemap = {-1: 0, 1: 1}
-
-    with open(path + 'wiki-RfA.txt','r') as f:
-        for i in range(n_edges_read):
-            # Read in the entry
-            # SRC:Guettarda
-            tail = f.readline().strip()[4:]
-
-            # TGT:Lord Roem
-            head = f.readline().strip()[4:]
-
-            # VOT:1
-            vote = int(f.readline().strip()[4:])
-
-            # RES:1
-            res = int(f.readline().strip()[4:])
-
-            # YEA:2013
-            year = int(f.readline().strip()[4:])
-
-            #DAT:19:53, 25 January 2013
-            date = f.readline().strip()[4:]
-
-            # TXT:'''Support''' per [[WP:DEAL]]: clueful, and unlikely to break Wikipedia.
-            txt = f.readline().strip()[4:]
-
-            # kill blank line
-            f.readline()
-
-            # Process the entry
-            # index users
-            if tail not in usermap:
-                usermap[tail] = u
-                u += 1
-            if head not in usermap:
-                usermap[head] = u
-                u += 1
-
-            if vote != 0:
-                # add vote edge
-                A[0, usermap[tail], usermap[head]] = vote
-
-                # add to incidence matrix
-
-                B[v, usermap[tail]] = 1
-                B[v, usermap[head]] = 1
-
-                # vectorize text using the hashing trick
-                #try:
-                #    commentcount[(tail,head)] += 1
-                #except KeyError:
-                #    commentcount[(tail,head)] = 1
-                #features = np.zeros(n_features)
-                #for token in txt.strip().split(' '):
-                #    features[hash(token) % n_features] += 1
-                #A[1:, usermap[tail], usermap[head]] = features
-
-                A[1:, usermap[tail], usermap[head]] = np.asarray(vectorizer.transform([txt]).todense())[0]
-
-                X[usermap[head],0] = resmap[res]
-
-                # treat the result as the node class
-                Y[v, votemap[vote]] = 1
-                v += 1
-
-    # normalize features
-    #for edge in commentcount:
-    #    A[1:, edge[0], edge[1]] /= commentcount[edge]
-
-    print u
-    print v
-
-    assert len(usermap) == n_nodes
-    assert u == n_nodes
-    assert v == n_edges
-
-    return A, B, X, Y
-
-
-
-
 
 
